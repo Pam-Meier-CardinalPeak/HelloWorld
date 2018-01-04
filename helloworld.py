23f59f1a415f2ec86483c62638f0144db6758b08
Pam-Meier-CardinalPeak <pmeier@cardinalpeak.com>
2018-01-04 12:13:54 -0700

    Added an add function to unnecessary_math with a logic error to see unittest fail.  Subtracted the numbers instead of adding them.

--- helloworld.py
+++ helloworld.py
@@ -1,5 +1,6 @@
 import unittest
 from unnecessary_math import multiply
+from unnecessary_math import add
 
 
 class HelloWorld(unittest.TestCase):
@@ -13,9 +14,15 @@ class HelloWorld(unittest.TestCase):
     def test_strings_a_3(self):
         self.assertEqual(multiply('a', 3), 'aaa')
 
+    def test_numbers_4_3(self):
+        self.assertEqual(add(4,3),7)
+
 
 if __name__ == '__main__':
     print("Hello World")
     print("Hello Galaxy")
     print("Hello Universe")
     unittest.main()
+
+
+
