61edc84cbd9434d0080602ab5165142b23ca39aa
Pam-Meier-CardinalPeak <pmeier@cardinalpeak.com>
2018-01-04 10:35:36 -0700

    Added another file and seeing if I can get them to run in Pycharm correctly.  Also, added an example unittests to also see how to setup the run configurations within PyCharm.

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,21 @@
-print("Hello World")
-print("Hello Galaxy")
-print("Hello Universe")
+import unittest
+from unnecessary_math import multiply
+
+
+class HelloWorld(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_numbers_3_4(self):
+        self.assertEqual(multiply(3, 4), 12)
+
+    def test_strings_a_3(self):
+        self.assertEqual(multiply('a', 3), 'aaa')
+
+
+if __name__ == '__main__':
+    print("Hello World")
+    print("Hello Galaxy")
+    print("Hello Universe")
+    unittest.main()
